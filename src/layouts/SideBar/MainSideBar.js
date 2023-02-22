495303aea45107c464359e80d72f965e74e2c258
wantop <wantop@jbnu.ac.kr>
2023-02-22 21:29:43 +0900

    feat : side menu styling

--- src/layouts/SideBar/MainSideBar.js
+++ src/layouts/SideBar/MainSideBar.js
@@ -1,15 +1,30 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 const MainSideBar = () => {
-    return(
+  return (
     <div className="side-bar">
-        <ul>
-            <li><Link to='/pageA'>page A</Link></li>
-            <li><Link to='/pageB'>page B</Link></li>
-            <li><Link to='/pageC'>page C</Link></li>
-        </ul>
+      <Menu vertical className="vh-100">
+        <Menu.Item>
+          <Menu.Header>Pages</Menu.Header>
+
+          <Menu.Menu>
+            <Menu.Item name="pageA">
+              <Link to="/pageA">page A</Link>
+            </Menu.Item>
+
+            <Menu.Item name="pageB">
+              <Link to="/pageB">page B</Link>
+            </Menu.Item>
+
+            <Menu.Item name="pageC">
+              <Link to="/pageC">page C</Link>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Item>
+      </Menu>
     </div>
-    )
-}
+  );
+};
 
 export default MainSideBar;
